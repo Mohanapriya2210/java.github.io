8689e8de07f2becbc91b0a42cab80b9d7258d4be
Mohanapriya2210 <156517311+Mohanapriya2210@users.noreply.github.com>
2024-02-01 14:34:09 +0530

    Add files via upload

---
+++ MainClass .java
@@ -0,0 +1,7 @@
+public class MainClass {
+public static void main(String[] args) {
+    System.out.println(Long.SIZE);
+    System.out.println(Double.SIZE);
+    System.out.println(Integer.SIZE);
+  }
+}
